be64249839a50e75084f3269d54d2ef9e981d0aa
Mayank Mehtani <mmehtani@ucsd.edu>
2016-12-29 13:15:20 +0000

    tweets printed with their sentiments

--- simple.sbt
+++ simple.sbt
@@ -9,6 +9,9 @@ libraryDependencies ++= Seq(
         "org.apache.spark" %% "spark-core" % "2.0.1",
         "org.apache.spark" %% "spark-streaming" % "2.0.1",
 	"databricks" % "spark-corenlp" % "0.2.0-s_2.10",
+	"org.apache.bahir" %% "spark-streaming-twitter" % "2.0.1",
+	"org.twitter4j" % "twitter4j-core" % "4.0.4",
+	"org.twitter4j" % "twitter4j-stream" % "4.0.4",
 	"org.apache.spark" %% "spark-sql" % "2.0.1",
 	"com.google.code.gson" % "gson" % "2.7",
         "edu.stanford.nlp" % "stanford-corenlp" % "3.6.0",
@@ -17,6 +20,3 @@ libraryDependencies ++= Seq(
 )
 
 
-/*"org.twitter4j" % "twitter4j-core" % "3.0.3",
-"org.twitter4j" % "twitter4j-stream" % "3.0.3",
-"org.apache.bahir" %% "spark-streaming-twitter" % "2.0.1",*/
--- src/main/scala/SimpleApp.scala
+++ src/main/scala/SimpleApp.scala
@@ -1,52 +1,80 @@
 /* SimpleApp.scala */
-
 import com.databricks.spark.corenlp.functions._
 import org.apache.spark.sql.functions._
 import org.apache.spark.SparkContext
-import org.apache.spark.sql.SparkSession
-
-object SimpleApp {
-	
-        def main(args: Array[String]) {
-
-		val sc = new SparkContext()
-
-		val sqlContext= new org.apache.spark.sql.SQLContext(sc)
-		import sqlContext.implicits._
-
-		val warehouseLocation = "/apps/hive/warehouse/spark-warehouse"
-		val spark = SparkSession
-		   .builder()
-		   .appName("SimpleApp")
-		   .config("spark.sql.warehouse.dir", warehouseLocation)
-		   .getOrCreate()
-		
-		val input = Seq((1, "<xml>Stanford University is located in California; It is a great university.</xml>")).toDF("id", "text")
-		val output = input.select(cleanxml('text).as('doc)).select(explode(ssplit('doc)).as('sen)).select('sen, tokenize('sen).as('words), ner('sen).as('nerTags), sentiment('sen).as('sentiment))
-		output.show(truncate = false)
-	
-
-	}
-
-}
-
-
-
-
-/*
-import org.apache.spark.SparkContext
-import org.apache.spark.SparkSession
+import org.apache.spark.sql.SparkSession 
 import org.apache.spark.SparkConf
 import org.apache.spark.streaming
-import org.apache.spark.sql.functions._
 import org.apache.spark.streaming._
 import org.apache.spark.streaming.StreamingContext._
 import org.apache.spark.streaming.twitter
 import org.apache.spark.streaming.twitter.TwitterUtils
 
 
+/* these imports are required to run the mysenti method  */
+import java.util.Properties
+import scala.collection.JavaConverters._
+import edu.stanford.nlp.ling.{CoreAnnotations, CoreLabel}
+import edu.stanford.nlp.neural.rnn.RNNCoreAnnotations
+import edu.stanford.nlp.pipeline.{CleanXmlAnnotator, StanfordCoreNLP}
+import edu.stanford.nlp.pipeline.CoreNLPProtos.Sentiment
+import edu.stanford.nlp.sentiment.SentimentCoreAnnotations
+import edu.stanford.nlp.simple.{Document, Sentence}
+import edu.stanford.nlp.util.Quadruple
+import org.apache.spark.sql.functions.udf
+
+
+object SimpleApp {
+
+	var sentimentPipeline: StanfordCoreNLP = _
+	def mysenti(sentence: String): (String, Int) = {
+
+        	if (sentimentPipeline == null) {
+
+                	val props = new Properties()
+                	props.setProperty("annotators", "tokenize, ssplit, parse, sentiment")
+                	sentimentPipeline = new StanfordCoreNLP(props)
+              	}
+
+                val annotation = sentimentPipeline.process(sentence)
+                val tree = annotation.get(classOf[CoreAnnotations.SentencesAnnotation]).asScala.head.get(classOf[SentimentCoreAnnotations.SentimentAnnotatedTree])
+                (sentence,RNNCoreAnnotations.getPredictedClass(tree))
+	}
+	
+        def main(args: Array[String]) {
+
+		val config = new SparkConf().setAppName("twitter-stream-sentiment")
+                val sc = new SparkContext(config)
+		sc.setLogLevel("WARN")
+                val ssc = new StreamingContext(sc, Seconds(5))
+
+                System.setProperty("twitter4j.oauth.consumerKey","po2YFywYNEkt0T02dB0Y7FLmK")
+                System.setProperty("twitter4j.oauth.consumerSecret","sl6iBv3Jcl59QXlpqXHdAw37BLagK2uFWoAe2WzbN3AH0dt0tS")
+                System.setProperty("twitter4j.oauth.accessToken","811565823127003137-oUMnVQOxJ0QreE4Gg4VDpCmMRDUYujh")
+                System.setProperty("twitter4j.oauth.accessTokenSecret","bufSJzBvgm1hfmOrcKocU7CNWjORzpped12RhudfNdq0c")
+		
+		val sqlContext = new org.apache.spark.sql.SQLContext(sc) // this is needed for the imports
+		import sqlContext.implicits._
+		
+		val warehouseLocation = "/apps/hive/warehouse/spark-warehouse"
+		val spark = SparkSession
+		   .builder()
+		   .appName("SimpleApp")
+		   .config("spark.sql.warehouse.dir", warehouseLocation)
+	   .getOrCreate()
+		
+		val stream = TwitterUtils.createStream(ssc, None)
+		val text_tweet = stream.map(status => status.getText()) //DStream returned
+		val text_tweet_senti = text_tweet.map(mysenti)
+
+		text_tweet_senti.print()		
+
+		ssc.start()
+		ssc.awaitTermination()
+			
+      }
+
+}
 
 
 
-
-*/
